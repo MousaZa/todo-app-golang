82fb90a66d8955717d29cce283dc166ef7ea673a
MousaZa <mous.zeydan@gmail.com>
2024-08-16 20:51:52 +0300

    Created Server

--- tasks-api/main.go
+++ tasks-api/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"github.com/MousaZa/todo-app/tasks-api/handlers"
 	"github.com/gorilla/mux"
 	"github.com/hashicorp/go-hclog"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -21,9 +25,28 @@ func main() {
 
 	sm.HandleFunc("/tasks/{id:[0-9]+}", h.UpdateTask).Methods(http.MethodPut)
 
-	err := http.ListenAndServe(":9091", sm)
-	if err != nil {
-		l.Error("Error starting Service", "error", err)
+	s := http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  30 * time.Second,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
 	}
+	
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Error("error starting server", err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Info("Received terminate, graceful shutdown", sig)
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 
 }
