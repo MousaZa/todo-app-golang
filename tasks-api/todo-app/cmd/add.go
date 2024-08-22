76da64093d2d810defe4983c7494d3f8c5644f8e
MousaZa <mous.zeydan@gmail.com>
2024-08-22 13:40:15 +0300

    Added add command

--- tasks-api/todo-app/cmd/add.go
+++ tasks-api/todo-app/cmd/add.go
@@ -4,7 +4,13 @@ Copyright © 2024 Mousa Zeydan <mous.zeydan@gmail.com>
 package cmd
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/MousaZa/todo-app/tasks-api/todo-app/data"
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
+	"net/http"
 )
 
 // addCmd represents the add command
@@ -17,8 +23,32 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
 
+	Run: func(cmd *cobra.Command, args []string) {
+		t := data.Task{Title: args[0], Description: args[1]}
+		j, err := json.Marshal(t)
+		if err != nil {
+			fmt.Printf("err :%v\n", err)
+			return
+		}
+		r := bytes.NewReader(j)
+		req, err := http.NewRequest(http.MethodPost, "http://localhost:9092/tasks", r)
+		if err != nil {
+			fmt.Printf("err :%v\n", err)
+			return
+		}
+		res, err := http.DefaultClient.Do(req)
+		if err != nil {
+			fmt.Printf("err :%v\n", err)
+			return
+		}
+		if res.StatusCode == http.StatusOK {
+			c := color.New(color.FgCyan)
+			c.Printf("Task was added successfuly\n")
+		} else {
+			r := color.New(color.FgRed)
+			r.Printf("Error adding task, code: %v\n", res.StatusCode)
+		}
 	},
 }
 
